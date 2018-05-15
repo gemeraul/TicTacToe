c981237bdd1a0cec657a3f4d1355ef76b74e9f9c
rgrimald <raul.grimaldi.barahona@intel.com>
2018-05-14 22:36:16 -0600

    Improved logic to add a mark

--- lib/Game.rb
+++ lib/Game.rb
@@ -8,8 +8,8 @@ class Game
     def initialize(title)
         @title = title
         @board = Board.new(3);
-        @human_player = Player.new("Raul", "X")
-        @computer_player = Player.new("R2D2","O")
+        @human_player = Player.new("Raul", "X", @board)
+        @computer_player = Player.new("R2D2","O", @board)
         @current_player = @human_player
         @turn = 0
     end
@@ -19,8 +19,7 @@ class Game
             puts
             puts "Turn: " + @turn.to_s + ", " + @current_player.name + " plays!"
             @board.print_board
-            @move_x, @move_y = @current_player.select_box
-            @board.add_mark(@move_x,@move_y,@current_player.mark)
+            @current_player.select_box
             @board.print_board
             #TODO: Add break when game is over
             switch_players
--- lib/Player.rb
+++ lib/Player.rb
@@ -2,15 +2,17 @@ class Player
 
     attr_accessor :name, :mark
 
-    def initialize(name, mark)
+    def initialize(name, mark, board)
         @name = name
         @mark = mark
+        @board = board
     end
 
     def select_box
         puts "#{@name}, its your turn! On which box would you like to add a #{@mark}? "
         puts "Enter your coordinates in the form: 'row' , 'column'. Eg: 1,1 for 1st box"
-        gets.strip.split(",").map(&:to_i)
+        @row, @column = gets.strip.split(",").map(&:to_i)
+        @board.add_mark(@row, @column, @mark)
     end
 
     #TODO: Add responsabilities functions
