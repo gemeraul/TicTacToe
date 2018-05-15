c9ba9cdde4b4a69d7455bff01e77c22068de79ea
rgrimald <raul.grimaldi.barahona@intel.com>
2018-05-14 23:56:06 -0600

    Added logic to ask for game configurtions and check game over

--- lib/Game.rb
+++ lib/Game.rb
@@ -7,23 +7,26 @@ class Game
 
     def initialize(title)
         @title = title
-        @board = Board.new(3);
-        @human_player = Player.new("Raul", "X", @board)
-        @computer_player = Player.new("R2-D2","O", @board)
+        set_up_board_size
+        create_name
+        select_mark
+        @board = Board.new(@size)
+        @human_player = Player.new(@name, @mark, @board)
+        @computer_player = Player.new("R2-D2",@computer_mark, @board)
         @current_player = @human_player
         @turn = 0
     end
 
     def play
-        #TODO: Add logic to ask for name and for which mark you want to use
         loop do
             puts
+            break if check_tie
             puts "Turn: " + @turn.to_s + ", " + @current_player.name + " plays!"
             puts "Current board status:"
             @board.print_board
             @current_player.select_box
-            #TODO: Add break when game is over
             switch_players
+            @turn+=1
             puts
             puts "Turn is over, switching players..."
         end
@@ -36,5 +39,31 @@ class Game
             @current_player = @human_player
         end
     end
+
+    def check_tie
+        if @turn >= @size**2
+            puts "Aww game is over, we have a draw!"
+            true
+        else
+            false
+        end
+    end
+
+    def set_up_board_size
+        puts "Type in the desired board size"
+        @size = gets.strip.to_i
+    end
+
+    def create_name
+        puts "Enter your name!"
+        @name = gets.strip
+    end
+
+    def select_mark
+        puts "Select your mark! (type X or O)"
+        @mark = gets.strip
+        @computer_mark = @mark == "X" ? "O" : "X"
+    end
+
 
 end
