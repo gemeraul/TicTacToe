ec1e117660b88bd0948268150c8c146bbafcfbaf
rgrimald <raul.grimaldi.barahona@intel.com>
2018-05-14 22:32:50 -0600

    Added function to switch players and loop on play

--- lib/Game.rb
+++ lib/Game.rb
@@ -15,13 +15,24 @@ class Game
     end
 
     def play
-        puts
-        puts "Turn: " + @turn.to_s + ", " + @current_player.name + " plays!"
-        @board.print_board
-        @move_x, @move_y = @current_player.select_box
-        @board.add_mark(@move_x,@move_y,@current_player.mark)
-        @board.print_board
-        # TODO: Switch turns and add loop
+        loop do
+            puts
+            puts "Turn: " + @turn.to_s + ", " + @current_player.name + " plays!"
+            @board.print_board
+            @move_x, @move_y = @current_player.select_box
+            @board.add_mark(@move_x,@move_y,@current_player.mark)
+            @board.print_board
+            #TODO: Add break when game is over
+            switch_players
+        end
+    end
+
+    def switch_players
+        if @current_player == @human_player
+            @current_player = @computer_player
+        else
+            @current_player = @human_player
+        end
     end
 
 end
