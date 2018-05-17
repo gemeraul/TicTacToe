188f5a1ff47af452e0e607358bd13ea4677e4038
rgrimald <raul.grimaldi.barahona@intel.com>
2018-05-17 10:59:09 -0600

    Adding Unit Tests for Computer class

---
+++ spec/Computer_spec.rb
@@ -0,0 +1,16 @@
+require "Computer"
+require "Board"
+
+RSpec.describe Computer, "select box" do
+    before :each do
+    @board = Board.new 3
+    @computer = Computer.new("TestingComputer", "O", @board)
+    allow(@board).to receive(:add_mark).and_return(true)
+    end
+    context "computer's turn to mark a box'" do
+        it "calls add_random_mark on board object" do
+            expect(@board).to receive(:add_random_mark)
+            @computer.select_box            
+        end
+    end
+end
