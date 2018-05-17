1b72335265b6ff457f2b0438da87745a8cda3e24
rgrimald <raul.grimaldi.barahona@intel.com>
2018-05-17 10:54:52 -0600

    Splitting functions for player class

--- lib/Player.rb
+++ lib/Player.rb
@@ -10,24 +10,28 @@ class Player
 
     def select_box
         loop do
-            puts "#{@name}, its your turn! On which box would you like to add a #{@mark}? "
-            puts "Enter your coordinates in the form: 'row' , 'column'. Eg: 1,1 for 1st box"
-            @row, @column = gets.strip.split(",").map(&:to_i)
-            if input_valid?
-                if @board.add_mark(@row-1, @column-1, @mark)
+            row, column = ask_for_coordinates
+            if input_valid?(row, column)
+                if @board.add_mark(row-1, column-1, @mark)
                     break
                 end
             end
         end
     end
 
-    def input_valid?
-        if @row.is_a?(Integer) && @column.is_a?(Integer)
+    def input_valid?(row, column)
+        if row.is_a?(Integer) && column.is_a?(Integer)
             true
         else
             puts "\nThis is an invalid input! Only use numbers separated by a comma >:(\n"
             false
         end
+    end
+
+    def ask_for_coordinates
+        puts "#{@name}, its your turn! On which box would you like to add a #{@mark}?\nEnter your coordinates in the form: 'row' , 'column'. Eg: 1,1 for 1st box"
+        coordinates = gets.strip.split(",").map(&:to_i)
+        coordinates
     end
 
 end
---
+++ spec/Player_spec.rb
@@ -0,0 +1,55 @@
+require "Player"
+require "Board"
+
+RSpec.describe Player, "input" do
+    before :each do
+    @board = Board.new 3
+    @player = Player.new("TestingPlayer", "X", @board)
+    end
+    context "user has entered coordinates" do
+        it "returns true when both are numbers" do       
+            expect(@player.input_valid?(1,1)).to be true
+        end
+        it "returns false when row is not a number" do       
+            expect(@player.input_valid?("NaN",1)).to be false
+        end
+        it "returns false when column is not a number" do       
+            expect(@player.input_valid?(1,"NaN")).to be false
+        end
+    end
+end
+
+RSpec.describe Player, "select box" do
+    before :each do
+    @board = Board.new 3
+    @player = Player.new("TestingPlayer", "X", @board)
+    allow(@player).to receive(:ask_for_coordinates).and_return([1,1])
+    allow(@board).to receive(:add_mark).and_return(true)
+    end
+    context "asking user to select box" do
+        it "calls add_mark on board object" do
+            expect(@board).to receive(:add_mark)
+            @player.select_box            
+        end
+    end
+    context "asking user to select box" do
+        it "calls add_mark n times until add_mark returns true" do
+            allow(@board).to receive(:add_mark).and_return(false, true)
+            expect(@board).to receive(:add_mark).twice
+            @player.select_box            
+        end
+    end
+end
+
+RSpec.describe Player, "ask for coordinates" do
+    before :each do
+    @board = Board.new 3
+    @player = Player.new("TestingPlayer", "X", @board)
+    allow(@player).to receive(:gets).and_return("1,1")
+    end
+    context "asking for user input" do
+        it "returns array of coordinates" do
+            expect(@player.ask_for_coordinates).to eq([1,1])            
+        end
+    end
+end
